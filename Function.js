81388da9d41167aeac67775067b5418e36d19b25
pranalipansare <pranalipasare@gmail.com>
2021-12-18 14:42:51 +0530

    first

---
+++ Function.js
@@ -0,0 +1,113 @@
+// // function Declartion
+// Syntax
+
+// function function_name(param){
+//     statemant
+// }
+
+
+// // function add (x, y) {
+// //     return x+y
+
+// // }
+
+// // let addition = add(30, 40)
+// // console.log(addition)
+
+
+// // function mul(p, q) {
+// //     return p * q
+// // }
+// // let multiplication = mul(4, 5)
+// // console.log (multiplication )
+
+
+
+// // function sub(a, b) {
+// //     return a - b
+// // }
+// // let subtraction = sub(60 - 30)
+// // console.log(subtraction)
+
+
+
+
+// // function expression
+// // syntax
+
+// let function_name= function(params) {
+//     statemant
+
+// }
+
+// // eg 1
+
+// // // // let add = function(x,y) {
+// // //     return x+y
+// // //  }
+// // //  console.log(add(3,4))
+
+
+// // eg.2
+
+// // //  let mul = function(s,p){
+// // //      return s*p
+// // //  }
+// // //  console.log(mul(8,9))
+
+// // eg.3
+
+// // //  let sub = function(p,q){
+// // //      return p-q
+// // //  }
+// //  console.log(sub(44,65))
+
+
+
+
+// //  function arrow 
+
+// // Syntax
+
+// //  let function_name = (params)=>{
+// //         statements
+// //  }
+
+// // let add= (a,b)=>{
+// // return a+b
+// // }
+// // console.log(add(70,80))
+
+
+// let sub = (x,y)=>{
+//     return x-y
+// }
+// console.log(sub(50,40))
+
+// let mul = (p,q)=>{
+//     return p*q  
+// }
+// console.log(mul(10,20))
+
+
+
+
+function add(x, y) {
+    return x + y
+}
+let pp = add(3, 4)
+console.log(pp)
+
+
+let add = function (p, q) {
+    return p + q
+}
+let qq = add(9, 7)
+console.log(qq)
+
+
+let add = (a, b) => {
+    return a + b
+}
+let oo = add(80, 50)
+console.log(oo)
